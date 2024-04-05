53fe3ef275977356d8da87e75b68778f8e63614b
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-04-05 16:50:17 +0200

    new updates 5/4/2024

--- resources/views/frontend/pages/pages_parts/page_desc.blade.php
+++ resources/views/frontend/pages/pages_parts/page_desc.blade.php
@@ -1,7 +1,8 @@
 <section id="intro" class="intro-section">
     <div class="intro-container wow fadeIn">
         <h1 class="mb-1 pb-0 company-title">ElAbed <span>Poultry</span></h1>
-        <p class="mb-4 pb-0 slogan">FROM OUR FARMS TO YOUR TABLE <br> THE BEST QUALITY YOU CAN FIND</p>
+        <p class="mb-4 pb-0 slogan">FROM OUR FARMS TO YOUR TABLE - THE BEST QUALITY YOU CAN FIND</p>
+        <p class="mb-4 pb-0 slogan-mob">FROM OUR FARMS TO YOUR TABLE <br> THE BEST QUALITY YOU CAN FIND</p>
 
     </div>
 </section>
