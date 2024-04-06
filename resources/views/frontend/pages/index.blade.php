f26bb86750a00069e7f36ae381c10576644b31d1
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-04-06 16:59:48 +0200

    new updates 6/4/2024

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -234,6 +234,7 @@
             <div class="intro-content">
                 <h1 class="mb-1 pb-0 company-title">ElAbed <span>Poultry</span></h1>
                 <p class="mb-4 pb-0 slogan">FROM OUR FARMS TO YOUR TABLE - THE BEST QUALITY YOU CAN FIND</p>
+                <p class="mb-4 pb-0 slogan-mob">FROM OUR FARMS TO YOUR TABLE <br> THE BEST QUALITY YOU CAN FIND</p>
             </div>
         </div>
 
