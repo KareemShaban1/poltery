0e54ef610554be376a854031a43fa7a8a6112b8d
KareemShaban1 <karim17196@fci.bu.edu.eg>
2024-04-22 15:08:50 +0200

    new updates 22/4/2024

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -26,22 +26,26 @@
                   flex-direction: column;
                   align-items: center;">
                       <h4 style="font-size:22px; font-family: 'Rubic'">{{ trans('frontend.Contact_Us') }}</h4>
-                      <div class="d-flex footer-item">
-                          <strong>{{ trans('frontend.Address') }}  : </strong>
-                          <div style="direction: ltr">
-                              {!! $website_info['Address'] ?? '' !!}
+                      <div style="display: flex;
+
+                      align-items: start;">
+                          <div class="d-flex footer-item">
+                              <strong>{{ trans('frontend.Address') }} : </strong>
+                              <div style="direction: ltr">
+                                  {!! $website_info['Address'] ?? '' !!}
+                              </div>
                           </div>
-                      </div>
-                      <div class="d-flex footer-item">
-                          <strong>{{ trans('frontend.Phone') }}  : </strong>
-                          <div style="direction: ltr">
-                              {!! $website_info['Phone'] ?? '' !!}
+                          <div class="d-flex footer-item">
+                              <strong>{{ trans('frontend.Phone') }} : </strong>
+                              <div style="direction: ltr">
+                                  {!! $website_info['Phone'] ?? '' !!}
+                              </div>
                           </div>
-                      </div>
-                      <div class="d-flex footer-item">
-                          <strong>{{ trans('frontend.Email') }}  : </strong>
-                          <div style="direction: ltr">
-                              {!! $website_info['Email'] ?? '' !!}
+                          <div class="d-flex footer-item">
+                              <strong>{{ trans('frontend.Email') }} : </strong>
+                              <div style="direction: ltr">
+                                  {!! $website_info['Email'] ?? '' !!}
+                              </div>
                           </div>
                       </div>
 
